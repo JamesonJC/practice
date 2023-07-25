85a03136e88b7cac56655e839f29e5799636aa30
Sabelodone <sabelozondo825gmail.com>
2023-07-25 13:01:05 -0700

    commit

--- _exit.c
+++ _exit.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-/#include "main.h"
-
 /**
  * shell_exit - Wait for a child process to terminate and handle exit status.
  * @pid: The process ID of the child process.
@@ -27,21 +25,4 @@ void shell_exit(pid_t pid, char **av)
                         exit(0);
                 }
         }
-}
-
-
-
-void shell_exit(pid_t pid, char **av)
-{
-	int state, exit_state;
-
-	waitpid(pid, &state, 0);
-	if (WIFEXITED(state))
-	{
-		exit_state = WEXITSTATUS(state);
-		if (exit_state == 0 && av[0] == NULL)
-		{
-			exit(0);
-		}
-	}
 }
