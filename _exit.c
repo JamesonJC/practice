221e9b10d14b882e63eb3c58a8b75fc34a42cc5e
Sabelodone <sabelozondo825gmail.com>
2023-07-26 06:53:49 -0700

    changes

--- _exit.c
+++ _exit.c
@@ -14,15 +14,15 @@
 
 void shell_exit(pid_t pid, char **av)
 {
-        int state, exit_state;
+	int state, exit_state;
 
-        waitpid(pid, &state, 0);
-        if (WIFEXITED(state))
-        {
-                exit_state = WEXITSTATUS(state);
-                if (exit_state == 0 && av[0] == NULL)
-                {
-                        exit(0);
-                }
-        }
+	waitpid(pid, &state, 0);
+	if (WIFEXITED(state))
+	{
+		exit_state = WEXITSTATUS(state);
+		if (exit_state == 0 && av[0] == NULL)
+		{
+			exit(0);
+		}
+	}
 }
